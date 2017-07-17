cd34ee6c39698fa419bf387d0d075a8824a7f7ce
mohan <mohan589@gmail.com>
2017-07-17 20:01:36 +0530

    changes for booking

--- app/controllers/bookings.js
+++ app/controllers/bookings.js
@@ -6,12 +6,12 @@ export default Ember.Controller.extend({
     actions:{
         save(){
             // const rental = this.modelFor('rental');
-        	let model = this.store.createRecord('booking', this.currentModel);
-        	// model.set('rental', rental);
-        	model.save().then(() => {
+            let model = this.store.createRecord('booking', this.currentModel);
+            // model.set('rental', rental);
+            model.save().then(() => {
                 alert('success');
-        		this.transitionToRoute('bookings.list', model);
-        	});
+                this.transitionToRoute('bookings.list', model);
+            });
         }
     }
 });
