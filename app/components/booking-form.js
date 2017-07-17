c1c82bbb55678c60e2493756fb6fc468b9232000
mohan <mohan589@gmail.com>
2017-07-17 20:39:08 +0530

    changes for booking

--- app/components/booking-form.js
+++ app/components/booking-form.js
@@ -1,15 +1,28 @@
 import Ember from 'ember';
 
 const BookingForm = Ember.Component.extend({
+	rental: Ember.computed('params.[]', function(){
+    	return this.get('params')[0];
+  	}),
+
+  	init:function(){
+  		this._super(...arguments);
+  		alert(this.get('rental'));
+  	}
+});
+
+BookingForm.reopenClass({
+  positionalParams: 'params',
+
+	didInsertElement(){
+		alert("invoked");
+	},
+
 	actions:{
 		save(){
 			this.attrs.save();
 		}
 	}
-});
-
-BookingForm.reopenClass({
-  positionalParams: 'rental',
 });
 
 export default BookingForm;
