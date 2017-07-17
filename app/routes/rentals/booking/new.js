14879cf1c726177677368140ff12d83176ba115a
mohan <mohan589@gmail.com>
2017-07-17 20:52:56 +0530

    changes for booking

--- app/routes/rentals/booking/new.js
+++ app/routes/rentals/booking/new.js
@@ -1,5 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-	controllerName: 'bookings'
+	controllerName: 'bookings',
+	// model(params) {
+ //        return this.store.findRecord('rental', params.id);
+ //    }
 });
