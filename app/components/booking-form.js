9d181df25bd7f2ed31541594848ac7bac353de13
mohan <mohan589@gmail.com>
2017-07-20 18:46:00 +0530

    added the new booking form

--- app/components/booking-form.js
+++ app/components/booking-form.js
@@ -5,6 +5,10 @@ export default Ember.Component.extend({
   	actions:{
 		save(){
 			this.attrs.save();
+		},
+
+		onSelectEntityType(rental){
+			this.get('booking').set('rental_id', rental);			
 		}
 	}
 });
