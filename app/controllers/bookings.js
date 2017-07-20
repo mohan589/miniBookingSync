2133692167dba1a2a798368108950bb7278babb3
mohan <mohan589@gmail.com>
2017-07-20 16:54:02 +0530

    added the new booking form

--- app/controllers/bookings.js
+++ app/controllers/bookings.js
@@ -1,18 +1,15 @@
 import Ember from 'ember';
 
-export default Ember.Controller.extend({    
-    newBookingModel: {},    
+export default Ember.Controller.extend({
+    newBookingModel: {},
 
     actions:{
         save(){
-            const rental = this.get('rental');
-            rental.get('bookings').addObject(this.get('model'));
-            // let model = this.store.createRecord('booking', this.get('model'));
-            // model.set('rental', rental);
-            rantal.save().then(() => {
-                alert('success');
-                this.transitionToRoute('bookings.list', this.get('model'));
-            });
+            let model = this.store.createRecord('booking', this.get('newBookingModel'))
+            this.set('newBookingModel', {})
+            model.save().then(()=>{
+                this.transitionToRoute('rentals.list');
+            })
         }
     }
 });
