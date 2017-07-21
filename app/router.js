178642a0cac34a4d642d720656e8007a0ce4b8b7
mohan <mohan589@gmail.com>
2017-07-21 20:31:56 +0530

    changes for bookings

--- app/router.js
+++ app/router.js
@@ -13,15 +13,15 @@ Router.map(function() {
     this.route('edit', { path: ':id/edit' });
     this.route('show', { path: ':id/show' });
     
-    this.route('booking', {path: ':rental_id/booking'}, function(){      
-      this.route('list');
-      this.route('new');
-    });
+    // this.route('booking', {path: ':rental_id/booking'}, function(){      
+    //   this.route('list');
+    //   this.route('new');
+    // });
   });
 
-  this.route('bookings', { path: 'rentals/:rental_id/bookings' }, function(){
-    this.route('list' , );
-    this.route('new', { path: '/new' });
+  this.route('bookings', function(){
+    this.route('new');
+    this.route('list' ,{ path: 'rentals/:rental_id/list' });
     this.route('edit', { path: ':id/edit' });
     this.route('show', { path: ':id/show' });
   });
--- app/routes/bookings.js
+++ app/routes/bookings.js
@@ -4,15 +4,15 @@ export default Ember.Route.extend({
 	controllerName: 'bookings',
 	
 	model(params){
-		return Ember.RSVP.hash({
-			booking: this.store.createRecord('booking'),			
-			rental: this.store.find('rental', params.rental_id),
-			bookings: this.store.find('rental', params.rental_id).get('bookings')
-		});
+		// return Ember.RSVP.hash({
+		// 	booking: this.store.createRecord('booking'),			
+		// 	rental: this.store.find('rental', params.rental_id),
+		// 	bookings: this.store.find('rental', params.rental_id).get('bookings')
+		// });
 	},
 
 	setupController(controller, model){
-		Ember.set(controller, 'rental', model.rental);
-		Ember.set(controller, 'bookings', model.bookings);
+		// Ember.set(controller, 'rental', model.rental);
+		// Ember.set(controller, 'bookings', model.bookings);
 	}
 });
