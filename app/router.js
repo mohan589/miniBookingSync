4596fdccfa5b7c99ea35d17e7324018095da6b66
mohan <mohan589@gmail.com>
2017-07-22 13:22:08 +0530

    changes for bookings list and new

--- app/router.js
+++ app/router.js
@@ -12,8 +12,8 @@ Router.map(function() {
     this.route('new');
     this.route('edit', { path: ':id/edit' });
     this.route('show', { path: ':id/show' });
-    
-    // this.route('booking', {path: ':rental_id/booking'}, function(){      
+
+    // this.route('booking', {path: ':rental_id/booking'}, function(){
     //   this.route('list');
     //   this.route('new');
     // });
@@ -21,8 +21,8 @@ Router.map(function() {
 
   this.route('bookings', function(){
     this.route('new');
-    this.route('list' ,{ path: 'rentals/:rental_id/list' });
-    this.route('edit', { path: ':id/edit' });
+    this.route('list' ,{ path: 'rentals/:rental_id/bookings_list' });
+    this.route('edit', { path: 'rentals/:rental_id/booking/:id/edit' });
     this.route('show', { path: ':id/show' });
   });
 });
--- app/routes/bookings.js
+++ app/routes/bookings.js
@@ -2,10 +2,10 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 	controllerName: 'bookings',
-	
+
 	model(params){
 		// return Ember.RSVP.hash({
-		// 	booking: this.store.createRecord('booking'),			
+		// 	booking: this.store.createRecord('booking'),
 		// 	rental: this.store.find('rental', params.rental_id),
 		// 	bookings: this.store.find('rental', params.rental_id).get('bookings')
 		// });
